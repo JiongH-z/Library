4863709f23c5fcb560e4f81a32539cdcef1a9949
Jiong <zjh200411@foxmail.com>
2026-07-20 15:51:56 +0800

    增加了保存到文件

--- Library.py
+++ Library.py
@@ -1,5 +1,8 @@
-﻿class ReaderLoanInformation:
-    def __init__(self,name,*lend_book):
+﻿import json
+import os
+
+class ReaderLoanInformation:
+    def __init__(self,name,lend_book):
         self.name = name
         self.lend_book = []
         for book in lend_book:
@@ -25,7 +28,38 @@
 
 class Library:
     def __init__(self):
-        self.all_reader_info = []
+        self.all_reader_info = []    #!存的全是对象
+        self.filepath = 'Library_info.json'
+        self.load_data()
+
+    def save_data(self):
+        data = []
+        for reader_info in self.all_reader_info:
+            data.append({
+                'name': reader_info.name,
+                'book_name': reader_info.lend_book
+            })
+        try:
+            with open(self.filepath, 'w', encoding='utf-8') as file:
+                json.dump(data, file, ensure_ascii=False, indent=4)
+                print(f'✅ 数据已保存到 {self.filepath}')
+        except Exception as e:
+            print(f'❌ 保存失败：{e}')
+
+    def load_data(self):
+        try:
+            if not os.path.exists(self.filepath):
+                with open(self.filepath, 'w') as file:
+                    json.dump([],file)
+            with open(self.filepath,'r',encoding='utf-8') as file:
+                data = json.load(file)
+
+                for item in data:
+                    reader_info = ReaderLoanInformation(item['name'],item['book_name'])
+                    self.all_reader_info.append(reader_info)
+                    print(f'✅ 成功从{self.filepath}中加载数据')
+        except Exception as e:
+            print(f'❌ 加载失败：{e}')
 
     def lend_book(self):
         print('进入借书流程')
@@ -38,6 +72,7 @@ class Library:
                 reader_info.add_loan_book(book_name)
                 print('已更新借书信息')
                 print(reader_info)
+                self.save_data()
                 return
 
         reader_info = ReaderLoanInformation(name,book_name)
@@ -45,6 +80,7 @@ class Library:
         print('已新建读者信息，借书成功')
         for reader_info in self.all_reader_info:
             print(reader_info)
+        self.save_data()
 
 
     def return_book(self):
@@ -54,35 +90,9 @@ class Library:
             if name == reader_info.name:
                 book_name = input('请输入归还的图书名字')
                 reader_info.reduce_lend_book(book_name)
+                self.save_data()
             else:
                 print('未查找到借阅者姓名')
-            for reader_info in self.all_reader_info:
-                print(reader_info)
-
-
-
-
-
-
-
-# class Book:
-#     def __init__(self,name,num):
-#         self.name = name
-#         self.num = num
-#
-#     def return_book(self):
-#         self.num += 1
-#     def lend_book(self):
-#         self.num -= 1
-
-
-
-
-
-if __name__ == '__main__':
-    library = Library()
-    library.lend_book()
-    library.lend_book()
-    library.return_book()
-
-
+                break
+        for reader_info in self.all_reader_info:
+            print(reader_info)
